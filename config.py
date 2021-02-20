905b8b6a6581551b8f987b072849c3855f49a4fb
posternutbag <40174207+posternutbag@users.noreply.github.com>
2021-02-20 00:47:00 -0500

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# .gitignore should include reference to config.py
+
+#AWS Credentials
+aws_access='XXX'
+aws_secret='XXX'
+s3bucket='XXX'
+
+#Ense Credentials
+enseKey = 'XXX'
+
+#Reddit credentials
+cID = 'XXX'
+cSC= 'XXX'
+userN = 'XXX'
+userP ='XXX'
